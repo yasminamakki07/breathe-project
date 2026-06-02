42be5a505551012dd2b9b81691390cce846ca090
yasminamakki07 <yasminamakki04@gmail.com>
2026-06-02 20:57:21 +0300

    Update SignupForm.js

--- breathe-frontend/src/components/SignupForm.js
+++ breathe-frontend/src/components/SignupForm.js
@@ -18,7 +18,7 @@ function SignupForm({ setUser }) {
     e.preventDefault();
 
     try {
-      const res = await axios.post("https://breathe-project-production.up.railway.app", formData);
+      const res = await axios.post("https://breathe-project-production.up.railway.app/api/auth/signup", formData);
       localStorage.setItem("user", JSON.stringify(res.data.user));
       setUser(res.data.user);
       window.location.href = "/breathing";
